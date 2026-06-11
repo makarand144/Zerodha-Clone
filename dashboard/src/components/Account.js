168a11d87c677f279087934e92aac03bc68aa6d9
makarand144 <156098532+makarand144@users.noreply.github.com>
2026-06-11 22:16:20 +0530

    Add Account component to display account details

--- dashboard/src/components/Account.js
+++ dashboard/src/components/Account.js
@@ -1,1 +1,18 @@
+import React from "react";
 
+const Account = () => {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Account Details</h1>
+
+      <hr />
+
+      <p>Client ID : Z123456</p>
+      <p>Account Status : Active</p>
+      <p>PAN Number : XXXXX1234X</p>
+      <p>KYC Status : Verified</p>
+    </div>
+  );
+};
+
+export default Account;
