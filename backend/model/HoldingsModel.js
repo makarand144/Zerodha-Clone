fb3521be142a152f73a9b14bd76086705ae54b1c
makarand144 <156098532+makarand144@users.noreply.github.com>
2026-06-11 22:03:54 +0530

    Create HoldingsModel using Mongoose schema

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,1 +1,7 @@
+const { model } = require("mongoose");
 
+const {HoldingSchema, HoldingsSchema} = require('../schemas/HoldingsSchema');
+
+const HoldingsModel = new model("holding", HoldingsSchema);
+
+module.exports = { HoldingsModel };
